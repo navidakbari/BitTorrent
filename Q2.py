d75a03f03251c7e14bc45a3ed105d867e2dd1432
navid <anavid.akbari@gmail.com>
2019-02-21 17:55:52 +0330

    this commit will add question 2 topology python file

---
+++ Q2.py
@@ -0,0 +1,28 @@
+from mininet.topo import Topo
+
+class MyTopo( Topo ):
+    def creatTopology( self ):
+        
+        Topo.__init__( self )
+        # Add hosts and switches
+        host1 = self.addHost( 'h1' )
+        host2 = self.addHost( 'h2' )
+        host3 = self.addHost( 'h3' )
+        host4 = self.addHost( 'h4' )
+        switch1 = self.addSwitch( 's1' )
+        switch2 = self.addSwitch( 's2' )
+        # Add links
+        self.addLink( host1, switch1 )
+        self.addLink( host2, switch1 )
+        self.addLink( switch1, switch2 )
+        self.addLink( host3, switch2 )
+        self.addLink( host4, switch2 )
+
+def main():
+    myTopo = MyTopo();
+    myTopo.creatTopology();
+
+if __name__ == "__main__":
+    main()
+
+        
