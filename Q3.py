129780b90a860514ac07a83cb7c4f61b868ee386
None <None>
2019-02-21 09:10:34 -0800

    this commit will add question 4 python file that make a 2 host and n switch

---
+++ Q3.py
@@ -0,0 +1,24 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+
+class TowHostMultiSwitch( Topo ):
+    def build( self, n=2 ):
+	host1 = self.addHost('host1')
+	host2 = self.addHost('host2')
+	switch = []
+	
+	for i in range(n):
+	    switch.append( self.addSwitch( 'switch%s'%(i+1)) )
+	
+	for i in range(n-1):
+	    self.addLink( switch[i], switch[i+1], bw = 1 , delay = '20ms', max_queue_size = 1 )
+
+mytopo = {'mytopo' : TowHostMultiSwitch}
+
+
+
+
