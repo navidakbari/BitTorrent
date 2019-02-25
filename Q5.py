15129d076fa881db114ae430f402cb9cdb4d6bb5
navid <anavid.akbari@gmail.com>
2019-02-25 17:50:15 +0330

    add Q5 file

---
+++ Q5.py
@@ -0,0 +1,27 @@
+import os
+import select
+import signal
+import struct
+import sys
+import time
+import socket,sys
+from impacket import ImpactPacket
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    while(True):
+        
+
+
+
+if __name__ == "__main__":
+    main()
