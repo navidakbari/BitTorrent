2af50b06454038ee55230732e6cf240786ca66f2
None <None>
2019-02-25 08:07:32 -0800

    Adding q5 to send to random ips

--- Q5.py
+++ Q5.py
@@ -1,26 +1,36 @@
 from ping import ping
 from ping import Ping
 from random import randint
+import random
 import commands
-
-numberOfClinet = 5
+import socket
+numberOfClient = 5
 
 def main():
     while(True):
 
         sourceIp = findRandomIp()
-		destinationIp = findRandomIp()
-		print "from %s to %s" % (sourceIp, destinationIp)
-        Server = Ping(sourceIp , destination)
+	destinationIp = findRandomIp()
+	while destinationIp == sourceIp:
+		 destinationIp = findRandomIp()
+	print "from %s to %s" % (sourceIp, destinationIp)
+        Server = Ping(sourceIp , destinationIp)
         Server.do_send()
         
-def findRandomIp():
-	ip = commands.getoutput('/sbin/ifconfig').split('\n')[1][27:28]
+def findRandomIp():   
+	hostname = socket.gethostname()    
+	#ip = socket.gethostbyname(hostname)
+	#print ip  
+	ip = commands.getoutput('/sbin/ifconfig').split('\n')[1][20:28]
 	ipFounded = False
+	newIpToJoin = ""
 	while not ipFounded :
-		newIpToJoin = randint(1, numberOfClinet)
-		newIpToJoin = "10.0.0." + str(numberOfClinet)
-		ipFounded = True if newIpToJoin != ip
+		newIpToJoin = randint(1, numberOfClient)
+		newIpToJoin = "10.0.0." + str(newIpToJoin)
+		if not newIpToJoin == ip:
+			ipFounded = True
+
+	return newIpToJoin
 		
 if __name__ == "__main__":
     main()
